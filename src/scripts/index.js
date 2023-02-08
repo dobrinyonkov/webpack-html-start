4017870973c270b0af026dfe002b2e26baa26e0a
Dobrin Dimchev <dobrin.dimchev@sap.com>
2023-02-08 17:55:45 +0200

    add ui5

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,12 @@
 import '../styles/index.scss';
+import '@ui5/webcomponents/dist/Button';
+import '@ui5/webcomponents/dist/DatePicker';
+import '@ui5/webcomponents/dist/Assets';
+
+import { setLanguage } from "@ui5/webcomponents-base/dist/config/Language.js";
+import { setTheme } from "@ui5/webcomponents-base/dist/config/Theme.js";
+
+setLanguage("es");
+setTheme("sap_horizon_dark");
 
 console.log('webpack starterkit');
